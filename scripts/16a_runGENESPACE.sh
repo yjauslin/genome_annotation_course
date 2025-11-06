10d543e07fae8099115ff81a384d7e0c2bf0f85d
yjauslin <yannick.jauslin@students.unibe.ch>
2025-11-06 09:26:06 +0100

    modified genespace files

--- scripts/16a_runGENESPACE.sh
+++ scripts/16a_runGENESPACE.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=pibu_el8           # IBU cluster partition 
 #SBATCH --cpus-per-task=20          # e.g. 20 
 #SBATCH --mem=64G                      # e.g. 64G or 200G 
-#SBATCH --time=0-04:00:00                    # format D-HH:MM, e.g. 2-00:00 
+#SBATCH --time=0-24:00:00                    # format D-HH:MM, e.g. 2-00:00 
 #SBATCH --output=/data/users/yjauslin/genome_annotation_course/logs/GENESPACE_%j.out 
 #SBATCH --error=/data/users/yjauslin/genome_annotation_course/logs/GENESPACE_%j.err
 
@@ -13,4 +13,4 @@ WORKDIR=/data/users/${USER}/genome_annotation_course
 chmod u+x $WORKDIR/scripts/16b_prepareGENESPACE.R
 
 apptainer exec --bind $COURSEDIR --bind $WORKDIR --bind $SCRATCH:/temp --bind /data \
-    $COURSEDIR/containers/genespace_latest.sif Rscript $WORKDIR/scripts/16b_prepareGENESPACE.R $WORKDIR/results/genespace
+    $COURSEDIR/containers/genespace_latest.sif Rscript $WORKDIR/scripts/16b_runGENESPACE.R $WORKDIR/results/genespace
